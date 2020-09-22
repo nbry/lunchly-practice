0eac98d154eb96bbf761d45b05efb3f86f5ef52f
nathan <nwbryant@umich.edu>
2020-09-22 16:25:47 -0700

    fullName function

--- models/customer.js
+++ models/customer.js
@@ -26,7 +26,7 @@ class Customer {
        FROM customers
        ORDER BY last_name, first_name`
     );
-    return results.rows.map(c => new Customer(c));
+    return results.rows.map((c) => new Customer(c));
   }
 
   /** get a customer by ID. */
@@ -51,6 +51,12 @@ class Customer {
     }
 
     return new Customer(customer);
+  }
+
+  // get customer's full name
+  fullName() {
+    let fullName = `${this.firstName} ${this.lastName}`;
+    return fullName;
   }
 
   /** get all reservations for this customer. */
